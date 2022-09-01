f8f5c1d8e8fdbfaea46d192eaa21ec917f205e37
Taeshawn Threatt <110959584+XP-Tekurai99@users.noreply.github.com>
2022-09-01 10:33:03 -0700

    Update deploy.js

--- Build a Web3 App with Solidity/Wave Portal/Scripts/deploy.js
+++ Build a Web3 App with Solidity/Wave Portal/Scripts/deploy.js
@@ -4,14 +4,14 @@ const main = async () =>
     const accountBalance = await deployer.getBalance();
 
 
-console.log("Deploying contracts with account: ", deployer.address);
+console.log("Contract Deployer Address: ", deployer.address);
 console.log("Account Balance: ", accountBalance.toString());
 
 const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 const waveContract = await waveContractFactory.deploy();
 await waveContract.deployed();
 
-console.log("WavePortal address: ", waveContract.address);
+console.log("WavePortal Address: ", waveContract.address);
 };
 
 const runMain = async () =>
