1d2de42110b56ad0c8d78cdc0d77a06a98125414
Taeshawn Threatt <110959584+XP-Tekurai99@users.noreply.github.com>
2022-09-16 06:32:01 -0700

    Update run.js

--- Build a Web3 App with Solidity/Wave Portal/Scripts/run.js
+++ Build a Web3 App with Solidity/Wave Portal/Scripts/run.js
@@ -3,21 +3,29 @@
 
 const main = async () =>
 {
-    const [owner, randomUser] = await hre.ethers.getSigners(); // gets the owner address and a random address
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); // compiles WavePortal and generates necessary files in the artifiacts directory
-    const waveContract = await waveContractFactory.deploy(); // hardhat makes a local ethereum network for WavePortal that is destroyed upon script completion
-    await waveContract.deployed(); // the constructor will wait to run until WavePortal is deployed to the local blockchain
+    const [owner, user] = await hre.ethers.getSigners(); // gets the owner address and a random address
+    const dreamContractFactory = await hre.ethers.getContractFactory("DreamPortal"); // compiles WavePortal and generates necessary files in the artifiacts directory
+    const dreamContract = await dreamContractFactory.deploy(); // hardhat makes a local ethereum network for WavePortal that is destroyed upon script completion
+    await dreamContract.deployed(); // the constructor will wait to run until WavePortal is deployed to the local blockchain
     
-    console.log("Contract deployed to: ", waveContract.address); // gives the address of the deployed contract
+    console.log("Contract deployed to: ", dreamContract.address); // gives the address of the deployed contract
     console.log("Contract deployed by:", owner.address); // outputs the owner address
     
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves(); // gets the total number of waves
+    let dreamCount;
+    dreamCount = await dreamContract.getTotalDreams(); // gets the total number of waves
+    console.log(dreamCount.toNumber());
 
-    let waveSent = await waveContract.wave();
-    await waveSent.wait();
+    let dreamTxn = await dreamContract.dream("I would like to be a witcher hunting a beast in Skellige.");
+    await dreamTxn.wait();
 
-    waveCount = await waveContract.getTotalWaves(); // checks if waveCount has changed
+    dreamCount = await dreamContract.getTotalDreams(); // checks if waveCount has changed
+
+    const [_, sender] = await hre.ethers.getSigners();
+    dreamTxn = await dreamContract.connect(sender).dream("Another experience I'd like to have is walking around Diamond City from Fallout 4.");
+    await dreamTxn.wait(); // waits for the transaction to be mined
+
+    let allDreams = await dreamContract.getAllDreams();
+    console.log(allDreams);
 };
 
 const runMain = async () =>
